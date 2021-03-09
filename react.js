b93bc2b78619dca1dd5daffcccc8688fb18570ad
Caleb Crosy Adaioo <calcronoo@gmail.com>
2021-03-09 23:24:36 +0000

    compute cells update value when inputs are changed

--- react.js
+++ react.js
@@ -1,18 +1,60 @@
+const allStoredCells = [];
 
 export class InputCell {
   constructor(value) {
     this.value = value;
+    this.outputCells = [];
   }
 
   setValue(value) {
     this.value = value;
+    this.outputCells.forEach((cell) => {
+      cell.getNewValue();
+    });
+
+    allStoredCells.forEach((storedCell) => {
+      storedCell.calculateNewValues();
+    });
+  }
+
+  updateOutput(cell) {
+    this.outputCells.push(cell);
+  }
+
+  calculateNewValues() {
+    this.callbackCells.forEach((cell) => {
+      cell.updateValue(this.value);
+    });
   }
 }
 
-export class ComputeCell {
+export class ComputeCell extends InputCell {
   constructor(inputCells = [], fn) {
+    super();
     this.inputCells = inputCells;
-    this.value = fn(this.inputCells);
+    this.callbackCells = [];
+    this.fn = fn;
+
+    inputCells.forEach((cell) => {
+      cell.updateOutput(this);
+    });
+
+    this.getNewValue();
+    allStoredCells.push(this);
+  }
+
+  getNewValue() {
+    this.value = this.fn(this.inputCells);
+    this.outputCells.forEach((cell) => {
+      cell.getNewValue();
+    });
+  }
+
+  set updateValue(value) {
+    if (value !== this.value) {
+      this.values.push(value);
+      this.value = value;
+    }
   }
 
   addCallback(cb) {
